b169486b59781cb4a82e75da5d919912e6125208
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2019-10-05 15:46:17 +0900

    add top-bar home-button

--- src/components/MKTopBar.vue
+++ src/components/MKTopBar.vue
@@ -1,31 +1,36 @@
 <template>
   <div>
-    <v-toolbar>
-      <v-toolbar-title>Made by Kiichi</v-toolbar-title>
+    <v-app-bar color="deep-purple accent-4" dark>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <router-link :to="{ name: 'home'}">
+        <v-toolbar-title>Made-by-Kiichi</v-toolbar-title>
+      </router-link>
 
       <div class="flex-grow-1"></div>
 
-      <v-toolbar-items>
-        <v-btn text>Career</v-btn>
-        <v-btn text>Act</v-btn>
-        <v-btn text>Links</v-btn>
-      </v-toolbar-items>
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
 
-      <template v-if="$vuetify.breakpoint.smAndUp">
-        <v-btn icon>
-          <v-icon>mdi-export-variant</v-icon>
-        </v-btn>
-        <v-btn icon>
-          <v-icon>mdi-delete-circle</v-icon>
-        </v-btn>
-        <v-btn icon>
-          <v-icon>mdi-plus-circle</v-icon>
-        </v-btn>
-      </template>
-    </v-toolbar>
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+      <v-menu left bottom>
+        <template v-slot:activator="{ on }">
+          <v-btn icon v-on="on">
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item v-for="n in 5" :key="n" @click="() => {}">
+            <v-list-item-title>Option {{ n }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
   </div>
 </template>
-
 
 <script lang="ts">
 import Vue from "vue";
