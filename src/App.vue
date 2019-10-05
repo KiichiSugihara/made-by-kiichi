1f14d488896afd40be1c7f6570ddb9752890b4da
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2019-10-05 14:52:24 +0900

    add top-bar

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,25 @@
 <template>
   <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-app-bar>
-
+    <m-k-top-bar></m-k-top-bar>
     <v-content>
-      <HelloWorld/>
+      <HelloWorld />
     </v-content>
   </v-app>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import Vue from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+import MKTopBar from "@/components/MKTopBar.vue";
 
 export default Vue.extend({
-  name: 'App',
+  name: "App",
   components: {
     HelloWorld,
+    MKTopBar
   },
   data: () => ({
     //
-  }),
+  })
 });
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,12 +3,14 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import HelloWorld from '../components/HelloWorld.vue';
+import Vue from "vue";
+import HelloWorld from "../components/HelloWorld.vue";
+import MKTopBar from "@/components/MKTopBar.vue";
 
 export default Vue.extend({
   components: {
     HelloWorld,
-  },
+    MKTopBar
+  }
 });
 </script>
