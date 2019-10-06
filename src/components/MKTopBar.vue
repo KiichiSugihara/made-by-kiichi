5a29984e3e35330cd402fcee03b029efa4cbaf22
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2019-10-06 16:31:35 +0900

    add(home): add top-bar navbutton/font

--- src/components/MKTopBar.vue
+++ src/components/MKTopBar.vue
@@ -1,33 +1,20 @@
 <template>
   <div>
     <v-app-bar :color="barColor" :flat='flat' dark >
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
       <router-link :to="{ name: 'home'}">
-        <v-toolbar-title class="title-button">Made-by-Kiichi</v-toolbar-title>
+        <v-toolbar-title class="title-button">Made by Kiichi</v-toolbar-title>
       </router-link>
 
       <div class="flex-grow-1"></div>
-
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-      <v-menu left bottom>
-        <template v-slot:activator="{ on }">
-          <v-btn icon v-on="on">
-            <v-icon>mdi-dots-vertical</v-icon>
-          </v-btn>
-        </template>
-
-        <v-list>
-          <v-list-item v-for="n in 5" :key="n" @click="() => {}">
-            <v-list-item-title>Option {{ n }}</v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-menu>
+       <router-link :to="{ name: 'home'}">
+        <v-toolbar-title class="nav-button">Act</v-toolbar-title>
+      </router-link>
+       <router-link :to="{ name: 'home'}">
+        <v-toolbar-title class="nav-button">Who</v-toolbar-title>
+      </router-link> <router-link :to="{ name: 'home'}">
+        <v-toolbar-title class="nav-button">Contact
+        </v-toolbar-title>
+      </router-link>
     </v-app-bar>
   </div>
 </template>
@@ -44,10 +31,25 @@ export default Vue.extend({
 </script>
 
 <style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css?family=Mansalva&display=swap');
 
 .title-button {
+  font-family: 'Mansalva', cursive;
   color: aliceblue;
 }
 // https://stackoverflow.com/questions/44808474/vue-router-how-to-remove-underline-from-router-link/56443074#56443074
 a {  text-decoration: none;}
+
+.nav-button {
+color: #fff;
+font-family: 'Mansalva', cursive;
+    display: block;
+    line-height: 1.6rem;
+    font-weight: 700;
+    outline: none!important;
+    padding: .4rem;
+    position: relative;
+    transition: color .25s cubic-bezier(.625,.125,.305,.875),opacity .25s cubic-bezier(.625,.125,.305,.875);
+padding: .675rem .8rem;
+}
 </style>>
