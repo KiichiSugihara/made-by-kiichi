f4b841d79abade1c4bef12db0c5592dff8214e0d
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2019-10-06 17:23:47 +0900

    add(pages): add who

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,6 @@
 import Act from '@/views/Act.vue'
 import Contact from '@/views/Contact.vue'
+import Who from '@/views/Who.vue'
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
@@ -7,23 +8,28 @@ import Home from './views/Home.vue'
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home,
-    },
-    {
-      path: '/act',
-      name: 'act',
-      component: Act,
-    },
-    {
-      path: '/contact',
-      name: 'contact',
-      component: Contact,
-    },
-  ],
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home,
+        },
+        {
+            path: '/act',
+            name: 'act',
+            component: Act,
+        },
+        {
+            path: '/who',
+            name: 'who',
+            component: Who,
+        },
+        {
+            path: '/contact',
+            name: 'contact',
+            component: Contact,
+        },
+    ],
 })
