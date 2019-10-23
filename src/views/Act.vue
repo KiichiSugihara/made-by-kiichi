59c70a43fb39357f97b4bc00258d0f614b82c280
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2019-10-23 23:35:06 +0900

    add(card): make cardComp

--- src/views/Act.vue
+++ src/views/Act.vue
@@ -3,16 +3,20 @@
     <m-k-top-bar></m-k-top-bar>
     <v-content class="content">
       <h1>Act</h1>
+      <m-k-card></m-k-card>
+      <m-k-card></m-k-card>
     </v-content>
   </v-app>
 </template>
 <script lang="ts">
 import MKTopBar from '@/components/MKTopBar.vue'
+import MKCard from '@/components/MKCard.vue'
 import Vue from 'vue'
 
 export default Vue.extend({
     components: {
         MKTopBar,
+        MKCard,
     },
 })
 </script>
